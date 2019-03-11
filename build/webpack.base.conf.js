ec59c118aaad9e08f6c374a10202e5e1ea11c977
chenzhuo <chenzhuo@topode.com>
2019-03-11 19:57:17 +0800

    更新
    更新

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -60,7 +60,7 @@ module.exports = {
         }
       },
       {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        test: /\.(png|jpe?g|gif)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
